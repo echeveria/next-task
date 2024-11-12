f81f5440f02581bab7957a92ec054d1b66463323
Stoyan Germanov <s.p.germanov@gmail.com>
2024-11-12 17:59:01 +0200

    add search, filter by price, css classes

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,18 +6,16 @@ import axios from 'axios';
 import 'datatables.net-bs5';
 import 'datatables.net-responsive-bs5';
 
-// Set Axios as a global object
-window.axios = axios;
 window.$ = window.jQuery = $;
 
-// Optional: Set up Axios default configurations (like CSRF token)
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 $(document).ready(function() {
-    console.log(1);
-    $('.display').DataTable({
+    const minEl = document.querySelector('#min');
+    const maxEl = document.querySelector('#max');
+
+    const table = $('#productTable').DataTable({
         ajax: {
-            url: '/api/get_products', // The route to fetch products data
+            url: '/api/get_products', 
             dataSrc: '' // Adjust based on the format of the returned JSON
         },
         columns: [
@@ -27,4 +25,29 @@ $(document).ready(function() {
             { data: 'price' }
         ]
     });
+    // Custom range filtering function
+table.search.fixed('range', function (searchStr, data, index) {
+    var min = parseInt(minEl.value, 10);
+    var max = parseInt(maxEl.value, 10);
+    var age = parseFloat(data.price) || 0; // use data for the age column
+    console.log(data.price);
+    if (
+        (isNaN(min) && isNaN(max)) ||
+        (isNaN(min) && age <= max) ||
+        (min <= age && isNaN(max)) ||
+        (min <= age && age <= max)
+    ) {
+        return true;
+    }
+ 
+    return false;
+});
+ 
+// Changes to the inputs will trigger a redraw to update the table
+minEl.addEventListener('input', function () {
+    table.draw();
+});
+maxEl.addEventListener('input', function () {
+    table.draw();
+});
 });
--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -7,9 +7,32 @@
 </head>
 
 <body>
-    <h1>Main page!</h1>
-    <div id="data-table">
-        <table class="display">...</table>
+    <div class="container">
+        <h1>Product List</h1>
+        <table class="inputs">
+            <tbody>
+                <tr>
+                    <td>Minimum price:</td>
+                    <td><input type="text" id="min" name="min" class="form-control form-control-sm"></td>
+                </tr>
+                <tr>
+                    <td>Maximum price:</td>
+                    <td><input type="text" id="max" name="max" class="form-control form-control-sm"></td>
+                </tr>
+            </tbody>
+        </table>
+        <table id="productTable" class="display table table-striped">
+            <thead>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Width</th>
+                    <th>Height</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
     </div>
 </body>
 

ed77872b57c8542e68ce29ee6364e71a5d8cf239
Stoyan Germanov <s.p.germanov@gmail.com>
2024-11-12 16:44:21 +0200

    add js libs and vite, create base table

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,30 @@
 import './bootstrap';
+
+import $ from 'jquery';
+import 'bootstrap';
+import axios from 'axios';
+import 'datatables.net-bs5';
+import 'datatables.net-responsive-bs5';
+
+// Set Axios as a global object
+window.axios = axios;
+window.$ = window.jQuery = $;
+
+// Optional: Set up Axios default configurations (like CSRF token)
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+$(document).ready(function() {
+    console.log(1);
+    $('.display').DataTable({
+        ajax: {
+            url: '/api/get_products', // The route to fetch products data
+            dataSrc: '' // Adjust based on the format of the returned JSON
+        },
+        columns: [
+            { data: 'product_name' },
+            { data: 'width' },
+            { data: 'height' },
+            { data: 'price' }
+        ]
+    });
+});
--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -3,10 +3,14 @@
 
 <head>
     <title>Example View</title>
+    @vite(['resources/css/app.css', 'resources/sass/app.sass', 'resources/js/app.js'])
 </head>
 
 <body>
     <h1>Main page!</h1>
+    <div id="data-table">
+        <table class="display">...</table>
+    </div>
 </body>
 
 </html>
